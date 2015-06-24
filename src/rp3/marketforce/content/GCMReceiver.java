4bc54d909d905fac69fef853c7fab70b2859b11d
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2015-06-24 10:38:23 -0500

    Correcciones

--- src/rp3/marketforce/content/GCMReceiver.java
+++ src/rp3/marketforce/content/GCMReceiver.java
@@ -1,23 +1,12 @@
 package rp3.marketforce.content;
 
-import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
-import org.w3c.dom.Text;
-
-import rp3.marketforce.MainActivity;
 import rp3.marketforce.R;
-import rp3.marketforce.StartActivity;
 import rp3.util.NotificationPusher;
 
 /**
