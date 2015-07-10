f7912a225f790dd6b0b0c3d91fcf8e303237723f
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2015-07-10 11:52:32 -0500

    Correccion a Marcacion de Ubicacion

--- src/rp3/marketforce/utils/Utils.java
+++ src/rp3/marketforce/utils/Utils.java
@@ -332,4 +332,19 @@ public class Utils {
         }
         return filename;
     }
+
+    public static int getDayOfWeek(Calendar cal)
+    {
+        switch (cal.get(Calendar.DAY_OF_WEEK))
+        {
+            case Calendar.MONDAY: return 1;
+            case Calendar.TUESDAY: return 2;
+            case Calendar.WEDNESDAY: return 3;
+            case Calendar.THURSDAY: return 4;
+            case Calendar.FRIDAY: return 5;
+            case Calendar.SATURDAY: return 6;
+            case Calendar.SUNDAY: return 7;
+            default: return 1;
+        }
+    }
 }
