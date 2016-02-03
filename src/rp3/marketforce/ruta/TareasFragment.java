ff965d6dcd1896bd8246aefc102c59a40b6fd4db
RP3-gmeregildo <gmeregildo@rp3.com.ec>
2016-02-03 10:35:22 -0500

    Correcciones

--- src/rp3/marketforce/ruta/TareasFragment.java
+++ src/rp3/marketforce/ruta/TareasFragment.java
@@ -7,8 +7,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import rp3.app.BaseFragment;
+import rp3.configuration.PreferenceManager;
+import rp3.marketforce.Contants;
 import rp3.marketforce.R;
 import rp3.marketforce.cliente.ClientDetailFragment;
+import rp3.marketforce.models.Agenda;
+import rp3.marketforce.models.AgendaTarea;
 import rp3.marketforce.models.AgendaTareaOpciones;
 import rp3.marketforce.models.Tarea;
 
@@ -23,88 +27,125 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
 public class TareasFragment extends BaseFragment {
-	 public interface EditTareasDialogListener {
-	        void onFinishTareasDialog(List<Tarea> tareas);
-	 }
+	public interface EditTareasDialogListener {
+		void onFinishTareasDialog(List<Tarea> tareas);
+	}
 
 	public static String ARG_TAREAS = "tareas";
+	public static String ARG_AGENDA = "agenda";
 	private LinearLayout Grupo;
 	private List<Tarea> tareas;
 	private ArrayList<String> respuestas;
-	
+	private long idAgenda = 0;
+
 	public static TareasFragment newInstance(List<Tarea> tareas) {
 		TareasFragment fragment = new TareasFragment();
 		ArrayList<String> strings = new ArrayList<String>();
-		for(Tarea tarea : tareas)
-		{
+		for (Tarea tarea : tareas) {
 			strings.add(tarea.getNombreTarea());
 		}
 		Bundle arguments = new Bundle();
 		arguments.putStringArrayList(ARG_TAREAS, strings);
 		fragment.setArguments(arguments);
 		return fragment;
-    }
+	}
 
-	 @Override
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	            Bundle savedInstanceState) {
-		 	respuestas = getArguments().getStringArrayList(ARG_TAREAS);
-		 
-	        View view = inflater.inflate(R.layout.fragment_dialog_tareas, container);
-	        
-	        Grupo = (LinearLayout) view.findViewById(R.id.tareas_container);
-	        
-	        getDialog().setTitle("Tareas");
-	        
-	        tareas = Tarea.getTareasVigentes(getDataBase());
-		    for(Tarea tarea: tareas)
-			{
-		    	CheckBox setter = new CheckBox(getActivity());
-		    	setter.setButtonDrawable(R.drawable.custom_checkbox);
-		    	if(respuestas != null)
-		    		setter.setChecked(existeRespuesta(tarea.getNombreTarea()));
-		    	setter.setText(tarea.getNombreTarea());
-		    	setter.setPadding(30, 15, 0, 15);
-				Grupo.addView(setter);
-			}
-		    
-		    ((Button) view.findViewById(R.id.actividad_aceptar)).setOnClickListener(new OnClickListener(){
+	public static TareasFragment newInstance(List<AgendaTarea> tareas, long idAgenda) {
+		TareasFragment fragment = new TareasFragment();
+		ArrayList<String> strings = new ArrayList<String>();
+		ArrayList<String> stringsRealizadas = new ArrayList<String>();
+		for (AgendaTarea tarea : tareas) {
+			if(tarea.getNombreTarea() != null)
+				strings.add(tarea.getNombreTarea());
+		}
+		Bundle arguments = new Bundle();
+		arguments.putStringArrayList(ARG_TAREAS, strings);
+		arguments.putLong(ARG_AGENDA, idAgenda);
+		fragment.setArguments(arguments);
+		return fragment;
+	}
 
-				@Override
-				public void onClick(View v) {
-					List<Tarea> respuestasTareas = new ArrayList<Tarea>();
-					for(int i = 0; i < Grupo.getChildCount();i++)
-					{
-						if(((CheckBox)Grupo.getChildAt(i)).isChecked())
-						{
-							respuestasTareas.add(tareas.get(i));
-						}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		respuestas = getArguments().getStringArrayList(ARG_TAREAS);
+
+		try {
+			idAgenda = getArguments().getLong(ARG_AGENDA);
+		}
+		catch(Exception ex)
+		{}
+
+		View view = inflater.inflate(R.layout.fragment_dialog_tareas, container);
+
+		Grupo = (LinearLayout) view.findViewById(R.id.tareas_container);
+
+		getDialog().setTitle("Tareas");
+
+		tareas = Tarea.getTareasVigentes(getDataBase());
+		for (Tarea tarea : tareas) {
+			CheckBox setter = new CheckBox(getActivity());
+			setter.setButtonDrawable(R.drawable.custom_checkbox);
+			if (respuestas != null && tarea.getIdTarea() != 0)
+				setter.setChecked(existeRespuesta(tarea.getNombreTarea()));
+			if (idAgenda != 0)
+				setter.setEnabled(!existeRespuesta(tarea.getNombreTarea()));
+			setter.setText(tarea.getNombreTarea());
+			setter.setPadding(30, 15, 0, 15);
+			Grupo.addView(setter);
+		}
+
+		((Button) view.findViewById(R.id.actividad_aceptar)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				List<Tarea> respuestasTareas = new ArrayList<Tarea>();
+				for (int i = 0; i < Grupo.getChildCount(); i++) {
+					if (((CheckBox) Grupo.getChildAt(i)).isChecked() && !existeRespuesta(tareas.get(i).getNombreTarea())) {
+						respuestasTareas.add(tareas.get(i));
 					}
+				}
+				if(idAgenda == 0) {
 					CrearVisitaFragment activity = (CrearVisitaFragment) getParentFragment();
-		            activity.onFinishTareasDialog(respuestasTareas);
+					activity.onFinishTareasDialog(respuestasTareas);
 					dismiss();
-				}});
-			
-			((Button) view.findViewById(R.id.actividad_cancelar)).setOnClickListener(new OnClickListener(){
-
-				@Override
-				public void onClick(View v) {
-					dismiss();
-				}});
-	 
-	        return view;
-	    }
-	 
-	 	public boolean existeRespuesta(String resp)
-		{
-			for(String r : respuestas)
-			{
-				if(r.equalsIgnoreCase(resp))
+				}
+				else
 				{
-					return true;
+					Agenda agd = Agenda.getAgenda(getDataBase(), idAgenda);
+					for(Tarea tarea : respuestasTareas) {
+						AgendaTarea agendaTarea = new AgendaTarea();
+						agendaTarea.setIdAgenda(agd.getIdAgenda());
+						agendaTarea.set_idAgenda(idAgenda);
+						agendaTarea.setEstadoTarea("P");
+						agendaTarea.setIdRuta(PreferenceManager.getInt(Contants.KEY_IDRUTA));
+						agendaTarea.setIdTarea(tarea.getIdTarea());
+						AgendaTarea.insert(getDataBase(), agendaTarea);
+					}
+					getParentFragment().onResume();
+					dismiss();
 				}
 			}
-			return false;
+		});
+
+		((Button) view.findViewById(R.id.actividad_cancelar)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dismiss();
+			}
+		});
+
+		return view;
+	}
+
+	public boolean existeRespuesta(String resp) {
+		for (String r : respuestas) {
+			if (r.equalsIgnoreCase(resp)) {
+				return true;
+			}
 		}
-	 
+		return false;
+	}
+
 }
